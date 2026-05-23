89dda442468a31f7cbad1e4b19bd6119aa177cf1
Fils Fortuné Cherfrere <cherfrerefilsfortune@hotmail.com>
2026-05-23 07:48:19 -0400

    Configurado entrypoint de Node y vercel.json para Express

--- server.js
+++ server.js
@@ -26,7 +26,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Arrancar el servidor
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
+// CRUCIAL PARA VERCEL: Exportamos la aplicación
+module.exports = app;
